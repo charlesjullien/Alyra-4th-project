17b866eafff1ae3998ef44fc5f13f4da4d4fb80a
charlesjullien <61630987+charlesjullien@users.noreply.github.com>
2023-02-22 05:38:15 +0100

    Update deploy.js

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -30,6 +30,22 @@ async function main() {
     "../frontend/src/artifacts/NftCollection_address.json",
     JSON.stringify({ address: nftCollection.address }, undefined, 2)
   );
+  
+    /*
+  // Interaction avec le contrat
+  const signer = (await hre.ethers.getSigners())[0]; // récupération du premier compte
+  const nftMarketplaceContract = new hre.ethers.Contract(nftMarketplace.address, nftMarketplace.interface, signer);
+
+  
+  let result = await nftMarketplaceContract.createNFTCollection("Test au deploiement", "TAD"); // appel de la fonction mint
+  console.log(`Collection created with transaction hash: ${result.hash}`);
+  collec = await nftMarketplaceContract.collectionDetails("TAD");
+  addrCollec = collec.collectionAddress;
+  result = await nftMarketplaceContract.createNFT("https://gateway.pinata.cloud/ipfs/QmYkPUwUZScqcv7byx3D93X5LSkfFU1EtPgWGifFumgAce", addrCollec, "TAD");
+  console.log(`Token created with transaction hash: ${result.hash}`);
+  let price = ethers.utils.parseEther(0.042);
+  result = await nftMarketplaceContract.listNFT(1, price);
+  console.log(`Token listed with transaction hash: ${result.hash}`);*/
 
 }
 
